96aaa46387ec1da46289fe779e3fb23d9d855a4c
Administrator <admin@example.com>
2018-10-16 14:27:46 +0800

    init

---
+++ src/main/java/com/thunisoft/masterdata/MasterdataApplication.java
@@ -0,0 +1,12 @@
+package com.thunisoft.masterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterdataApplication.class, args);
+    }
+}
