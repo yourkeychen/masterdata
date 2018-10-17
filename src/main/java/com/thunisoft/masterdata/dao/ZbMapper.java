21146c37b10095ea1e0c712bcd4078c1dc33f194
zhangweijun <zhangweijun_sh@thunisoft.com>
2018-10-17 19:05:16 +0800

    添加logback 和与driud集成

--- src/main/java/com/thunisoft/masterdata/dao/ZbMapper.java
+++ src/main/java/com/thunisoft/masterdata/dao/ZbMapper.java
@@ -2,11 +2,8 @@ package com.thunisoft.masterdata.dao;
 
 import com.thunisoft.masterdata.model.Zb;
 
-import java.util.List;
-
 public interface ZbMapper {
     int insert(Zb record);
 
     int insertSelective(Zb record);
-
 }
