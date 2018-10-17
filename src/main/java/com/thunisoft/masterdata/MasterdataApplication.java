0acb9cfc809f1ed4b0248b1a5ed9f6127a712059
guowei <guowei_sh@thunisoft.com>
2018-10-17 14:22:31 +0800

    mybatis扫描jar包

--- src/main/java/com/thunisoft/masterdata/MasterdataApplication.java
+++ src/main/java/com/thunisoft/masterdata/MasterdataApplication.java
@@ -1,8 +1,10 @@
 package com.thunisoft.masterdata;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "com.thunisoft.masterdata.dao")
 @SpringBootApplication
 public class MasterdataApplication {
 
