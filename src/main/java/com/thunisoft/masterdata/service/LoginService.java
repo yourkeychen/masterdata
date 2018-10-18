ca4a06d4b840cdec123a700ad6a307126ce03685
guowei <guowei_sh@thunisoft.com>
2018-10-18 09:41:39 +0800

    shiro框架，登陆逻辑

---
+++ src/main/java/com/thunisoft/masterdata/service/LoginService.java
@@ -0,0 +1,248 @@
+/*
+ * @(#)CommonService.java 2017年12月27日下午2:06:50
+ * fzba
+ * Copyright 2017 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.thunisoft.masterdata.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.thunisoft.masterdata.pojo.LoginBean;
+import com.thunisoft.masterdata.pojo.MenuBean;
+import com.thunisoft.masterdata.shiro.token.MyToken;
+import com.thunisoft.masterdata.util.ControllerTool;
+import com.thunisoft.masterdata.util.PropertiesUtil;
+import com.thunisoft.masterdata.util.RequestUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * CommonService
+ * @author jiangyishan
+ * @version 1.0
+ *
+ */
+@Service("LoginService")
+public class LoginService {
+
+    private static Logger logger = LoggerFactory.getLogger(LoginService.class);
+    
+    /**
+     * 登录session集合 
+     */
+    private static Map<String,HttpSession> logins = new HashMap<String,HttpSession>();
+    
+    /**
+     * 获取登录信息
+     * @return
+     */
+    public JSONObject getLoginMessage(){
+        JSONObject loginMessage = null;
+        if(ControllerTool.isLogin()){
+        	LoginBean user = ControllerTool.getCurrentUser();
+        	loginMessage = new JSONObject();
+        	loginMessage.put("id", user.getZybm());
+        	loginMessage.put("login", user.getAccount());
+        	loginMessage.put("name", user.getUsername());
+        }
+        return loginMessage;
+    }
+    
+    /**
+     * 登录状态
+     * @param username
+     * @return
+     */
+    public boolean isActive(String username){
+        HttpSession httpSession = logins.get(username);
+        try {
+            Object value = httpSession.getAttribute("user");
+			if(httpSession!=null&&value!=null){
+                return true;
+            }
+        } catch (Exception e) {
+            logger.info(username,"已销毁session");
+        }
+        return false;
+    }
+    
+    /**
+     * 伪单点登录的入口
+     * @param username 用户名
+     */
+    public void singleLogin(String username){
+        if(!ControllerTool.isLogin()){
+        	loginSSO(username);
+        }
+    }
+    
+    /**shiro的登录,未验证任何东西,本系统登陆成功后,调用此方法
+	 * @param zybm
+	 * @param loginIp
+	 */
+	public void loginSSOForShiro(String zybm,String loginIp) {
+		Subject subject = SecurityUtils.getSubject();
+		MyToken token = new MyToken(zybm, "", loginIp, "SSO");
+		try {
+			subject.login(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**shiro的登录,未验证任何东西,本系统登陆成功后,调用此方法
+	 * @param loginIp
+	 */
+	public void loginForShiro(String username, String password,String loginIp) {
+		Subject subject = SecurityUtils.getSubject();
+		MyToken token = new MyToken(username, "D41D8CD98F00B204E9800998ECF8427E", loginIp, "account");
+		try {
+			subject.login(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+    /**
+	 * 单点登录逻辑
+	 *
+	 * @return
+	 */
+	public Boolean loginSSO(String zybm) {
+		String loginIP = getLoginIP();
+		LoginBean user = LoginBusiness.loginByUserCode(zybm, loginIP);
+		return dealSessionAndLoginForShiro(user);
+	}
+	/**
+	 * 单点登录逻辑
+	 *
+	 * @return
+	 */
+	public JSONArray allLoginSSO(String zybm) {
+		String loginIP = getLoginIP();
+		JSONArray result =new JSONArray();
+		LoginBean user = LoginBusiness.loginByUserCode(zybm, loginIP);
+		if (user==null) {
+			return null;
+		}
+		String allSystemPrivilege= PropertiesUtil.getProperty("all.system.privilege", "wdfh,cbpz,hshc,hssw,hydl");
+		String[] systemArr =allSystemPrivilege.split(",");
+		if (systemArr==null||systemArr.length==0) {
+			return null;
+		}
+		for (String str : systemArr) {
+			if(ControllerTool.hasSystemQx(user, str, false)){
+				JSONObject tmp =new JSONObject();
+				tmp.put("code", str);
+				tmp.put("name",PropertiesUtil.getProperty(str));
+				String totalAddress = PropertiesUtil.getProperty("totalAddress");
+				String systemIp = PropertiesUtil.getProperty("system.ip");
+				String address =systemIp+str+"/"+str+totalAddress+zybm;
+				tmp.put("address",address);
+				result.add(tmp);
+			}
+		}
+		return result;
+	}
+	/**
+	 * 登录逻辑
+	 * 
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	public Boolean login(String username, String password) {
+		String loginIP = getLoginIP();
+		LoginBean user = LoginBusiness.loginByAccountAndPassword(username, password, loginIP);
+		return dealSessionAndLoginForShiro(user);
+	}
+	/**登陆后处理session和继续登陆shiro
+	 * @param user
+	 * @return
+	 */
+	private Boolean dealSessionAndLoginForShiro(LoginBean user) {
+		HttpSession session = getSession(false);
+		if (user != null) {
+			String zybm = user.getZybm();
+			String username =user.getAccount();
+			// 他处登陆挤掉
+			HttpSession httpSession = logins.get(zybm);
+			if (httpSession != null&&httpSession!=session) {
+				try {
+					httpSession.invalidate();
+				} catch (Exception e) {
+					logger.info(zybm, "已销毁session");
+				}
+			}
+			session.setAttribute("user", user);
+			loginForShiro(zybm, "",username);
+			logins.put(zybm, session);
+			return true;
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * 通过request获取 用户的ip
+	 * 
+	 * @return ip
+	 */
+	private String getLoginIP() {
+		return getRequest().getRemoteAddr();
+	}
+	/**
+	 * 通过RequestContextHolder获取request
+	 * 
+	 * @return request
+	 */
+	private HttpServletRequest getRequest() {
+		return RequestUtil.getRequest();
+	}
+	/**
+	 * 通过request获取session
+	 * 
+	 * @return session
+	 */
+	private HttpSession getSession(boolean flag) {
+		return getRequest().getSession(flag);
+	}
+
+	public JSONArray getMenu() {
+		JSONArray result =new JSONArray();
+		JSONObject single = null;
+		List<MenuBean> currentUserMenu = ControllerTool.getCurrentUserMenu();
+		if (currentUserMenu==null) {
+			single=new JSONObject();
+			result.add(single);
+			return null;
+		}
+		for (MenuBean menuBean : currentUserMenu) {
+			single=new JSONObject();
+			single.put("name",menuBean.getMenuName());
+			single.put("url",menuBean.getMenuAddress());
+			result.add(single);
+		}
+        return result;
+	}
+	public Boolean getSingleMenu(String menuStr) {
+		List<MenuBean> currentUserMenu = ControllerTool.getCurrentUserMenu();
+		if (currentUserMenu==null) {
+			return false;
+		}
+		Boolean menuQX = ControllerTool.getMenuQXCF(currentUserMenu, menuStr);
+        return menuQX;
+	}
+	
+}
---
+++ src/main/java/com/thunisoft/masterdata/util/ControllerTool.java
@@ -0,0 +1,210 @@
+package com.thunisoft.masterdata.util;
+
+
+import com.alibaba.druid.util.StringUtils;
+import com.thunisoft.masterdata.pojo.LoginBean;
+import com.thunisoft.masterdata.pojo.MenuBean;
+import org.apache.commons.collections.CollectionUtils;
+
+
+import javax.servlet.http.HttpSession;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ControllerTool {
+	
+	
+	public static boolean isLogin() {
+		LoginBean currentUser = getCurrentUser();
+		if (currentUser!=null) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	/**
+	 * 获取登陆人sessionBean
+	 * @return
+	 */
+	public static LoginBean getCurrentUser() {
+		HttpSession session = RequestUtil.getSession(false);
+		if (session==null) {
+			return null;
+		}
+		LoginBean currentUser = (LoginBean) session.getAttribute("user");
+		return currentUser;
+	}
+	
+	/**
+	 * 获取登陆人的职员编码
+	 * @return
+	 */
+	public static String getCurrentUserId() {
+		LoginBean currentUser = ControllerTool.getCurrentUser();
+		if (currentUser!=null) {
+			return currentUser.getZybm();
+		}
+		return null;
+	}
+	
+	/**获取登陆人的在权限系统中配置的菜单
+	 * @return 
+	 */
+	public static List<MenuBean> getCurrentUserMenu() {
+		LoginBean currentUser = ControllerTool.getCurrentUser();
+		if (currentUser!=null) {
+			return currentUser.getMenu();
+		}
+		return null;
+	}
+	
+	 /**获取登陆人的在单个菜单是否有权限 系统拆分前的方法
+	 * @param currentUserMenu 菜单list
+	 * @param menuName 需判断的菜单名称
+	 * @param flag 是否递归遍历所有子菜单 false只判断1级菜单
+	 * @return
+	 */
+	public static Boolean getMenuQX(List<MenuBean> currentUserMenu,String menuName,boolean flag) {
+		Boolean menuQX =false;
+		if (CollectionUtils.isEmpty(currentUserMenu)|| StringUtils.isEmpty(menuName)) {
+			return false;
+		}
+		for (MenuBean menuBean : currentUserMenu) {
+			if (menuName.equals(menuBean.getMenuName())) {
+				return true;
+			}
+			if (flag) {
+				List<MenuBean> menus = menuBean.getMenus();
+				if (CollectionUtils.isNotEmpty(menus)) {
+					menuQX= getMenuQX(menus, menuName,flag);
+				}
+				if (true==menuQX) {
+					return menuQX;
+				}
+			}
+		}
+		return menuQX;
+	}
+
+
+
+	/**登陆时使用,判断user是否有本某个子系统的权限
+	 * @param user 用户对象
+	 * @param systemPrivilegeCode 需要判断的子系统代码
+	 * @param flag 是否递归遍历所有菜单,true是,false 只判断下探到1级菜单权限
+	 * @return
+	 */
+	public static Boolean hasSystemQx(LoginBean user,
+			String systemPrivilegeCode,boolean flag) {
+		if (user==null) {
+			return false;
+		}
+		List<MenuBean> currentUserMenu = user.getMenu();
+		Boolean menuQX =false;
+		menuQX = getMenuQX(currentUserMenu, systemPrivilegeCode, flag);
+		return menuQX;
+	}
+	
+	
+	/**获取登陆人的在单个菜单是否有权限 ,系统拆分后的方法
+	 * @return 
+	 */
+	public static Boolean getMenuQXCF(List<MenuBean> currentUserMenu,String menuName) {
+		String systemPrivilegeCode = PropertiesUtil.getProperty("system.privilege", "");
+		Boolean menuQX =false;
+		if (CollectionUtils.isEmpty(currentUserMenu)||StringUtils.isEmpty(menuName)) {
+			return false;
+		}
+		//先获取对应系统的子菜单
+		MenuBean menuBeanByName = getMenuBeanByName(currentUserMenu,systemPrivilegeCode);  
+		if (menuBeanByName==null) {
+			return false;
+		}
+		ArrayList<MenuBean> currentUserMenu2 = new ArrayList<MenuBean>();
+		currentUserMenu2.add(menuBeanByName);
+		menuQX = getMenuQX(currentUserMenu2, menuName,true);
+		return menuQX;
+	}
+	/**获取登陆人的在单个菜单是否有权限
+	 * @return 
+	 */
+	/**从菜单对象集合中 通过 菜单名称 获取指定的菜单对象(集合的元素或者元素的子元素)
+	 * @param currentUserMenu 菜单对象集合
+	 * @param menuName 目标菜单的菜单名称 
+	 * @return 指定的菜单对象
+	 */
+	public static MenuBean getMenuBeanByName(List<MenuBean> currentUserMenu,String menuName) {
+//		String systemPrivilegeCode = ArteryConfigUtil.getProperty("system.privilege", "");
+		MenuBean resultMenuBean =null;
+		if (CollectionUtils.isEmpty(currentUserMenu)||StringUtils.isEmpty(menuName)) {
+			return null;
+		}
+		for (MenuBean menuBean : currentUserMenu) {
+			if (menuName.equals(menuBean.getMenuName())) {
+				return menuBean;
+			}
+			List<MenuBean> menus = menuBean.getMenus();
+			if (CollectionUtils.isNotEmpty(menus)) {
+				resultMenuBean= getMenuBeanByName(menus, menuName);
+			}
+			if (null!=resultMenuBean) {
+				return resultMenuBean;
+			}
+		}
+		return resultMenuBean;
+	}
+	/**
+	 * 将一个map集合转化成JavaBean
+	 * @param type
+	 * @param map
+	 * @return
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws InvocationTargetException
+	 */
+	public static Object converMap(Class type,Map map) 
+	    		throws IntrospectionException, IllegalAccessException,
+	    		InstantiationException,InvocationTargetException{
+	    	BeanInfo beanInfo = Introspector.getBeanInfo(type);
+	    	Object obj = type.newInstance();
+	    	
+	    	PropertyDescriptor [] propertyDescriptors = beanInfo.getPropertyDescriptors();
+	    	for (int i = 0; i< propertyDescriptors.length; i++){
+	    		PropertyDescriptor descriptor = propertyDescriptors[i];
+	    		String propertyName = descriptor.getName();
+	    		if(map.containsKey(propertyName)){
+	    			Object value = map.get(propertyName);
+	    			
+	    			Object[] args = new Object[1];
+	    			
+	    			descriptor.getWriteMethod().invoke(obj, args);
+	    		}
+	    	}
+			return obj;
+	}
+	
+//	/**根据职员编码查找部门
+//	 * @param zybm
+//	 * @return
+//	 */
+//	public static String getCurrentDept(String zybm) {
+//		String dsName = "dataSource";
+//		DataSourceInfo dsi = ArteryDataSourceUtil.getDataSourceService().getDataSourceById(dsName);
+//		DataSource dataSource = dsi.getDataSource();
+//		JdbcTemplate jdbcTemplate =new JdbcTemplate(dataSource);
+//		String sql ="SELECT b.c_name FROM \"public\".t_aty_user A, \"public\".t_aty_dept b " +
+//				"WHERE A .c_dept = b.c_id AND A .c_id = ? LIMIT 1 OFFSET 0";
+//		String queryForObject = jdbcTemplate.queryForObject(sql, new Object[]{zybm}, String.class);
+//		return queryForObject;
+//	}
+	
+}
