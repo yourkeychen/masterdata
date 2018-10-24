024c1c048da230f399c3d480934bbc3832a948b1
zhangweijun <zhangweijun_sh@thunisoft.com>
2018-10-24 18:42:45 +0800

    layui后台管理页面

--- src/main/java/com/thunisoft/TestController.java
+++ src/main/java/com/thunisoft/TestController.java
@@ -1,15 +1,23 @@
 package com.thunisoft;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController
+@Controller
+@RequestMapping("/masterdata")
 public class TestController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello world,现在是:"+(new Date()).toLocaleString();
+    }
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
     }
 }
--- src/main/java/com/thunisoft/shiro/config/ShiroConfig.java
+++ src/main/java/com/thunisoft/shiro/config/ShiroConfig.java
@@ -30,6 +30,7 @@ public class ShiroConfig {
       Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
       // 配置不会被拦截的链接 顺序判断，因为前端模板采用了thymeleaf，这里不能直接使用 ("/static/**", "anon")来配置匿名访问，必须配置到每个静态目录
       filterChainDefinitionMap.put("/pdjk/css/**", "anon");
+      filterChainDefinitionMap.put("/masterdata/**","anon");
 //      filterChainDefinitionMap.put("/fonts/**", "anon");
         filterChainDefinitionMap.put("pdjk//images/**", "anon");
         filterChainDefinitionMap.put("/pdjk/js/**", "anon");
@@ -41,7 +42,7 @@ public class ShiroConfig {
       filterChainDefinitionMap.put("/pdjk/svg", "logout");
       //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
       //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-      filterChainDefinitionMap.put("/**", "authc");
+      filterChainDefinitionMap.put("/pdjk/**", "authc");
       // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
       shiroFilterFactoryBean.setLoginUrl("/pdjk/html/login.html");
       // 登录成功后要跳转的链接
